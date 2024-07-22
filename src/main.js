013eb85b1d19fb9e44775dab84bd016d728ec2d0
NikolayPutyata <nikolayputyata@gmail.com>
2024-07-22 20:49:26 +0300

    step 2

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import addImagesToHtml from './js/render-functions';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.form');
 
